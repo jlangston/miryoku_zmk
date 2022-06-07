5f330623eaea450b712c8cb2eb8cd02786684344
Jeff Langston <jeff@prsgrp.dev>
2022-06-07 15:44:16 -0600

    6 column corne

--- miryoku/mapping/42/corne.h
+++ miryoku/mapping/42/corne.h
@@ -6,15 +6,15 @@
 #define XXX &none
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+     K00,  K01,  K02,  K03,  K04,  K05,      K06,  K07,  K08,  K09,  K10,  K11, \
+     K12,  K13,  K14,  K15,  K16,  K17,      K18,  K19,  K20,  K21,  K22,  K23, \
+     K24,  K25,  K26,  K27,  K28,  K29,      K30,  K31,  K32,  K33,  K34,  K35, \
+     N36,  N37,  N38,  K39,  K40,  K41,      K42,  K43,  K44,  N45,  N46,  N47  \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+K00  K01  K02  K03  K04  K05       K06  K07  K08  K09  K10  K11 \
+K12  K13  K14  K15  K16  K17       K18  K19  K20  K21  K22  K23 \
+K24  K25  K26  K27  K28  K29       K30  K31  K32  K33  K34  K35 \
+               K39  K40  K41       K42  K43  K44
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
