d0b9d0c83e754265f6bca66e7a02438af273ac49
Jeff Langston <jeff@prsgrp.dev>
2023-05-07 14:31:22 -0600

    semi over quote

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -116,7 +116,7 @@ U_NP,       U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV,
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 &kp GRAVE,  &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                           &kp BSLH,  \
-&kp LCTRL,  U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),                 &kp SQT,   \
+&kp LCTRL,  U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),                 &kp SQT,   \
 &kp LSHFT,  U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),           &kp COLON, \
 U_NP,       U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP,                            U_NP
 
